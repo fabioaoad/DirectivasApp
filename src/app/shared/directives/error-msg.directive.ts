4e1a4469e2690c6fa5e4a1d23c317e9614e09fda
Fabio <fabioaoad@gmail.com>
2021-11-17 19:02:41 -0300

    propiedades color y otroMenjare como privadas - Directivas Personalizadas - Sección 23, apartado 10

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -11,18 +11,20 @@ export class ErrorMsgDirective implements OnInit, OnChanges {
   private _otroMensaje: string = 'complete este campo';
 
   @Input() set color( valor: string ){
-    this.htmlElement.nativeElement.style.color = valor;
+    // this.htmlElement.nativeElement.style.color = valor;
     this._color = valor;
+    this.setColor();
   }
   //@Input() color : string = 'red';
 
-  @Input() mensaje: string = 'Este campo es necesario';
+  // @Input() mensaje: string = 'Este campo es necesario';
   //@Input() otroMensaje: string = 'complete este campo';
 
   @Input() set otroMensaje( valor: string ){
     //console.log(valor);
-    this.htmlElement.nativeElement.innerText = valor;
-    this.otroMensaje = valor;
+   // this.htmlElement.nativeElement.innerText = valor;
+    this._otroMensaje = valor;
+    this.setOtroMensaje();
   }
 
 
@@ -51,25 +53,27 @@ export class ErrorMsgDirective implements OnInit, OnChanges {
     //  console.log('NgOnInit  directive');
     // console.log(this.color);       //undefined
     // console.log(this.otroMensaje); //undefined
-    // this.setColor();
+     this.setColor();
     // this.setMensaje();
-    // this.setOtroMensaje();
+     this.setOtroMensaje();
      this.setEstilo();
   }
 
 
-  // setColor(): void {
-  //   //this.htmlElement.nativeElement.style.color = 'red';
-  //   this.htmlElement.nativeElement.style.color = this.color;
-  // }
-
-  setMensaje(): void {
-    this.htmlElement.nativeElement.innerText = this.mensaje;
+  setColor(): void {
+    //this.htmlElement.nativeElement.style.color = 'red';
+    //this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.style.color = this._color;
   }
 
-  // setOtroMensaje(): void {
-  //   this.htmlElement.nativeElement.innerText = this.otroMensaje;
+  // setMensaje(): void {
+  //   this.htmlElement.nativeElement.innerText = this.mensaje;
   // }
+
+  setOtroMensaje(): void {
+    //this.htmlElement.nativeElement.innerText = this.otroMensaje;
+    this.htmlElement.nativeElement.innerText = this._otroMensaje;
+  }
 
   setEstilo(): void {
     this.htmlElement.nativeElement.classList.add('form-text');
