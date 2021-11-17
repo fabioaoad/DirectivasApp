542c50b3f6362382adabd77e805c9c107fabd1a7
Fabio <fabioaoad@gmail.com>
2021-11-17 11:43:55 -0300

    primer commit

---
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styles: [
+  ]
+})
+export class AgregarComponent {
+
+
+  constructor(  ) { }
+
+
+}
---
+++ src/app/shared/directives/error-msg.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Directive({
+  selector: '[error-msg]'
+})
+export class ErrorMsgDirective implements OnInit {
+
+
+  ngOnInit(): void {
+  }
+
+
+
+}
