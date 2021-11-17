9a2429a6f7a7e5611abab7989c69ac93cb489791
Fabio <fabioaoad@gmail.com>
2021-11-17 17:39:33 -0300

    Cambiar el mensaje de la etiqueta - Directivas Personalizadas - Sección 23, apartado 8

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -6,8 +6,9 @@ import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '
 export class ErrorMsgDirective implements OnInit {
 
 
-  htmlElement    : ElementRef<HTMLElement>;
-  @Input() color : string = 'red';
+  htmlElement     : ElementRef<HTMLElement>;
+  @Input() color  : string = 'red';
+  @Input() mensaje: string = 'Este campo es necesario';
 
 
   constructor( private  elem: ElementRef<HTMLElement>) {
@@ -20,10 +21,9 @@ export class ErrorMsgDirective implements OnInit {
   ngOnInit(): void {
   //  console.log('NgOnInit  directive');
     this.setColor();
+    this.setMensaje();
+    this.setEstilo();
   }
-
-
-
 
 
   setColor(): void {
@@ -31,5 +31,12 @@ export class ErrorMsgDirective implements OnInit {
     this.htmlElement.nativeElement.style.color = this.color;
   }
 
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this.mensaje;
+  }
+
+  setEstilo(): void {
+    this.htmlElement.nativeElement.classList.add('form-text');
+  }
 
 }
